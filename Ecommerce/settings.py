b0608dff6b9f5e1b467ffeb213d855d4f5c32a3f
Nguyen Thanh Chung <nguyenthanhchung1012004@gmail.com>
2025-02-28 16:55:47 +0700

    Review and Rating

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -127,13 +127,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT= BASE_DIR /'static'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT= BASE_DIR /'static'
+# STATICFILES_DIRS = [
+#     'Ecommerce/static/',
+
+# ]
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'Ecommerce/static',
 
